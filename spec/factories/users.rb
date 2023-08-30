18115dd6c75b6eeda68187bb878d14ada18957eb
daria <kapoor.darya@gmai.com>
2023-08-30 08:26:12 +0300

    Add tests to user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,12 @@
 
 FactoryBot.define do
   factory :user do
+    first_name { Faker::Name.first_name }
+    email { Faker::Internet.unique.email }
+    password { Faker::Internet.password(min_length: 6, max_length: 20) }
+
+    trait :with_last_name do
+      last_name { Faker::Name.last_name }
+    end
   end
 end
