00e50d3b53d5389ae3fdad03c503f1dd3c99804c
daria <kapoor.darya@gmai.com>
2023-09-14 21:13:35 +0300

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/user'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'main#home'
 end
