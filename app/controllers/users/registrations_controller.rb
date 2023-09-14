eb0ddc7334602028d6326632033e88b67d0ca312
daria <kapoor.darya@gmai.com>
2023-09-14 21:26:41 +0300

    Add user view

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,4 +59,28 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
+  # POST /resource
+  def create(*args, **kwargs)
+    super
+  end
+
+  def update(*args, **kwargs)
+    super
+  end
+
+  private
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
 end
