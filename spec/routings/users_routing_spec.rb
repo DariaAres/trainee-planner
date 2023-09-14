3be0d558b89d7503364ca22ad6e8da957b686a62
daria <kapoor.darya@gmai.com>
2023-09-14 22:24:10 +0300

    Add tests

---
+++ spec/routings/users_routing_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Users routings', type: :routing do
+  it 'path to sign up' do
+    expect(get('/users/sign_up')).to route_to(controller: 'devise/registrations', action: 'new')
+  end
+
+  it 'path to sign in' do
+    expect(get('/users/sign_in')).to route_to(controller: 'users/sessions', action: 'new')
+  end
+
+  it 'path to edit' do
+    expect(get('/users/edit')).to route_to(controller: 'users/registrations', action: 'edit')
+  end
+end
