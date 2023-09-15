7f78f355c706fd9edf0dd015058172d1415a03e3
Daria Kapoor <120682364+DariaAres@users.noreply.github.com>
2023-09-15 17:33:31 +0300

    Revert "Add category model"
    This reverts commit 4848b9b1c51b06b281456e87c1e40551a18d620c.

--- app/models/category.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Category < ApplicationRecord
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
-end
--- db/seeds/development.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-%w[Personal Work Rest].each { |category| Category.find_or_create_by!(name: category) }
