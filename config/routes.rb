17caa9dee2962a9b5e0fcb17387fe2eeac614c88
daria <kapoor.darya@gmai.com>
2023-09-16 12:37:53 +0300

    Add categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'category/index'
+  get 'category/show'
+  get 'category/create'
+  get 'category/update'
+  get 'category/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
