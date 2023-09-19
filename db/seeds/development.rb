51390db601fb1efaf8d0ef3d8ebe224ff4c17931
Daria Kapoor <120682364+DariaAres@users.noreply.github.com>
2023-09-19 11:17:46 +0300

    Task: Add category model to develop branch (#25)

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+%w[Personal Work Rest].each { |category| Category.find_or_create_by!(name: category) }
+
 5.times do |index|
   User.find_or_create_by(email: "user_#{index}@example.com") do |user|
     user.first_name = Faker::Name.first_name
