04fed8ee6881b5c6d9d030232a8c37d7ef082b1e
daria <kapoor.darya@gmai.com>
2023-09-29 20:44:22 +0300

    Add service for counting user events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require './app/services/user_events_service'
+
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @events_counter = UserEventsService.new(user: current_user) if current_user
+  end
 end
