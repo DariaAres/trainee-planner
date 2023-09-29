7cd3d569057e1ee9b3ee170dea23f3f7f377c2fb
daria <kapoor.darya@gmai.com>
2023-09-29 20:44:22 +0300

    smth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require './app/services/user_events_service'
+
 class HomeController < ApplicationController
   def index
     @events_counter = UserEventsService.new(user: current_user) if current_user
