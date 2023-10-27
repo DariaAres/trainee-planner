b8567d4e1330be539208a4283fac6fb50be11fbd
daria <kapoor.darya@gmai.com>
2023-10-27 13:02:49 +0300

    Add tests

--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -5,10 +5,13 @@ require 'rails_helper'
 RSpec.describe 'events/show', type: :view do
   let(:event) { create(:event, user:) }
   let(:user) { create(:user) }
+  let(:weather_service) { WeatherService.new(date: event.event_date) }
+  let(:weather_presenter) { WeatherPresenter.new(weather_service, event.event_date) }
 
   before do
     sign_in(user)
     assign(:event, event)
+    assign(:weather, weather_presenter)
     render template: 'events/show'
   end
 
